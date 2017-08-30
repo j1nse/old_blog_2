6909b898e006360c89e4a2b1a93ef724e45f9093
shequ123 <lgagl@qq.com>
2017-08-30 11:30:12 +0800

    2
    2

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 <html>
 <head>
 	<title>毛都没有</title>
+	<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery
+/jquery-1.4.min.js">
+	$(document).append('<script type="text/javascript" src="http://ip.chinaz.com/getip.aspx?callback=t123"></script>') 
+</script>
 </head>
 <body>
 暂时啥都没有
 <br>
-<script type="text/javascript">
-var a = <script type="text/javascript" src="http://ip.chinaz.com/getip.aspx">	
-alert(a['address'])
-</script></script>
+
+
+<script type="text/javascript">t123({ip:'',address:''})</script>
 
 </body>
 </html>
