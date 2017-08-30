85a3028096713f6176d7a9e4f8bcacd2017045a3
shequ123 <lgagl@qq.com>
2017-08-30 11:08:21 +0800

    1
    1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>毛都没有</title>
+</head>
+<body>
+暂时啥都没有
+<br>
+<script type="text/javascript">
+var a = <script type="text/javascript" src="http://ip.chinaz.com/getip.aspx">	
+alert(a['address'])
+</script></script>
+
+</body>
+</html>
